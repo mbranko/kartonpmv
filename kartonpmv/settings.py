1021ae59609b8eac925703297062713eeda7ed9c
Branko Milosavljevic <mbranko@uns.ac.rs>
2016-12-22 22:51:34 +0100

    Sredjen debug toolbar.

--- kartonpmv/settings.py
+++ kartonpmv/settings.py
@@ -162,3 +162,4 @@ LOGGING = {
     }
 }
 
+INTERNAL_IPS = ['127.0.0.1', ]
--- kartonpmv/urls.py
+++ kartonpmv/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
