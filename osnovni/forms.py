cc86aa47506745f66dc2a35a8b27098740dfd056
Branko Milosavljevic <mbranko@uns.ac.rs>
2017-02-15 13:56:24 +0100

    Forma radi dobro sa datumom.

--- osnovni/forms.py
+++ osnovni/forms.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.forms import ModelForm
+from django.forms import ModelForm, DateField, DateInput
 from osnovni.models import MuzejskiPredmet
 
 
 class PredmetForm(ModelForm):
+    datum_nastanka = DateField(widget=DateInput(format='%d.%m.%Y.'), input_formats=('%d.%m.%Y.',), required=False)
+
     class Meta:
         model = MuzejskiPredmet
         fields = '__all__'
